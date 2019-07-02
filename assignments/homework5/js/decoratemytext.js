c6e535e75fe82a662284be32dd4d3884a103ea1a
limaoxuan <563157030@qq.com>
2019-07-01 21:27:57 -0500

    up

--- assignments/homework5/js/decoratemytext.js
+++ assignments/homework5/js/decoratemytext.js
@@ -1,5 +1,6 @@
 let timerID = null;
 let size = getFontSize();
+
 function myClick() {
     // alert("Hello, world!");
     if (timerID == null) {
@@ -7,30 +8,52 @@ function myClick() {
         size = parseInt(size);
         console.log(size);
         timerID = setInterval(changeFontSize, 500);
-    }else {
+    } else {
         // setInterval()
         clearTimeout(timerID);
         timerID = null;
     }
 
 
-
 }
 
 function changeFontSize() {
 
-console.log(size);
+    console.log(size);
     size = size + 2;
-    document.getElementById("my-textarea").style.fontSize = size+ "px";
+    document.getElementById("my-textarea").style.fontSize = size + "px";
 
 }
 
 function getFontSize() {
     let elem = document.getElementById("my-textarea");
-    return  window.getComputedStyle(elem, null).getPropertyValue("font-size");
+    return window.getComputedStyle(elem, null).getPropertyValue("font-size");
 }
 
 document.getElementById("my-click").onclick = myClick;
+document.getElementById("convert").onclick = convert;
+
+function convert() {
+    let element = document.getElementById("my-textarea");
+    console.log(element);
+    element.value = translate( element.value);
+}
+
+function translate(str) {
+    str=str.toLowerCase();
+    var n =str.search(/[aeiuo]/);
+    switch (n){
+        case 0: str = str+"way"; break;
+        case -1: str = str+"ay"; break;
+        default :
+            //str= str.substr(n)+str.substr(0,n)+"ay";
+            str=str.replace(/([^aeiou]*)([aeiou])(\w+)/, "$2$3$1ay");
+            break;
+    }
+    return str;
+
+}
+
 let isChecked = false;
 
 function myChange() {
