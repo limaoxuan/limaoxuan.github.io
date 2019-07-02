14227194402f0a02fdaca9a716f5369257253eb6
limaoxuan <563157030@qq.com>
2019-07-01 21:03:44 -0500

    add new feature

--- assignments/homework5/js/ascii.js
+++ assignments/homework5/js/ascii.js
@@ -3,10 +3,12 @@ let index = 0;
 let clearNumber;
 let speed;
 function start() {
+    "use strict";
     if (clearNumber) {
         clearInterval(clearNumber);
     }
-
+    document.getElementById("start").disabled = true;
+    document.getElementById("stop").disabled = false;
     let animationType = document.getElementById("animation").value;
 
     let size = document.getElementById("size").value;
@@ -23,16 +25,22 @@ function start() {
 }
 
 function stop() {
+    "use strict";
     if (clearNumber) {
         clearInterval(clearNumber);
+
     }
+    document.getElementById("start").disabled = false;
+    document.getElementById("stop").disabled = true;
     let element = document.getElementById("my-textarea");
     let animationType = document.getElementById("animation").value;
     element.value = ANIMATION[animationType].split("=====\n")[0];
+
 }
 
 function animation(element, array) {
     // console.log(str);
+    "use strict";
     element.value = array[index];
     index = (index + 1) % array.length;
 }
